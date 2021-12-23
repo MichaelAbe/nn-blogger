8ee88df2ec888d484e0a33c6d7c0b1e17f06249a
MichaelAbe <michael.k.abe@gmail.com>
2021-12-23 12:15:53 -0500

    created and rendered navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import Navbar from './Components.js/Navbar';
 
 function App() {
+
+  const title = 'Welcome to the new blog'
+  const likes = 50
+
   return (
     <div className="App">
+      <Navbar></Navbar>
      <div className="content">
-       <h1>App Component</h1>
+       <h1>{title}</h1>
+       <p>liked {likes} times</p>
      </div>
     </div>
   );
